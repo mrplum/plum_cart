0d3ce149bf15aa3237514b0b82eaec146a6514c6
EriGoldenberg <erikagoldenberg@gmail.com>
2020-10-08 08:40:52 -0300

    showing a card in gridlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-//import './App.css';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
@@ -7,6 +6,7 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 //import StarBorderIcon from '@material-ui/icons/StarBorder';
 import data from './data.json';
+import Card from './components/Card';
 
 
 
@@ -17,11 +17,15 @@ const useStyles = makeStyles((theme: Theme) =>
       flexWrap: 'wrap',
       justifyContent: 'space-around',
       overflow: 'hidden',
-      backgroundColor: theme.palette.background.paper,
+      backgroundColor: 'rgb(131, 155, 167)'/*theme.palette.background.paper*/,
     },
     gridList: {
-      width: 500,
-      height: 450,
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      display: 'flex',
+      flexWrap: 'wrap',
+      width: 700,
+      height: 700,
       // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
       transform: 'translateZ(0)',
     },
@@ -40,10 +44,18 @@ function App() : JSX.Element {
   const classes = useStyles();
   return (
 <div className={classes.root}>
-<GridList cellHeight={200} spacing={1} className={classes.gridList}>
+<GridList cellHeight={400} spacing={1} className={classes.gridList}>
   {data.map((shirt) => (
     <GridListTile key={shirt.title} cols={shirt.featured ? 2 : 1} rows={shirt.featured ? 2 : 1}>
-      <img src={shirt.img} alt={shirt.title} />
+      <div>
+          <Card
+                img={shirt.img}
+                title={shirt.title}
+                price={shirt.price}
+                dark = {false}
+
+          />
+      </div>
       <GridListTileBar 
         title={shirt.title}
         titlePosition="top" 
