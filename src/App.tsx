235871121b98886476bb22c7f5462012ab279c18
EriGoldenberg <erikagoldenberg@gmail.com>
2020-10-08 19:56:33 -0300

    switch button added but without functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,42 @@ import React from 'react';
 import ImageList from './components/List/ImageList';
 import CardList from './components/List/CardList';
 import data from './data.json';
+import Switch, { SwitchClassKey, SwitchProps } from '@material-ui/core/Switch';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { purple } from '@material-ui/core/colors';
 
 /*pasar data con props */
 /*sombra a card cuando cursor */
-function App() : JSX.Element {
+
+interface IProps {
+}
+
+interface IState {
+  card : boolean;
+}
+
+class App extends React.Component<IProps,IState>{
+
+  constructor(props: IProps){
+    super(props);
+    this.state = {
+      card : true
+    };
+  }
+
+  render (): JSX.Element {
   return (
     <div >
-      <CardList data={data} />
+      <FormGroup>
+      <FormControlLabel
+        control={<Switch checked={this.state.card} onChange={() =>{this.setState ({card : !this.state.card});}} />}
+        label="Card"
+      />
+    </FormGroup>
     </div>
   );
+}
 }
 
 export default App;
