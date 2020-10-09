7b7874f2509207060d3162d91a7fd4beff5fb880
yani-c <yaniceli1d@gmail.com>
2020-10-09 10:01:54 -0300

    styles for switch and CardList

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,32 @@ import React from 'react';
 import ImageList from './components/List/ImageList';
 import CardList from './components/List/CardList';
 import data from './data.json';
-import Switch, { SwitchClassKey, SwitchProps } from '@material-ui/core/Switch';
+import Switch from '@material-ui/core/Switch';
+import { withStyles } from '@material-ui/core/styles';
+import {indigo } from '@material-ui/core/colors';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import style from './App.module.css';
-/*pasar data con props */
-/*sombra a card cuando cursor */
 
 interface IProps {}
 
 interface IState {
   card : boolean;
 }
+
+const BlueSwitch = withStyles({
+  switchBase: {
+    color: indigo[300],
+    '&$checked': {
+      color: indigo[500],
+    },
+    '&$checked + $track': {
+      backgroundColor: indigo[500],
+    },
+  },
+  checked: {},
+  track: {},
+})(Switch);
 
 class App extends React.Component<IProps,IState>{
 
@@ -31,8 +45,9 @@ class App extends React.Component<IProps,IState>{
         <h1>The Shirt Store</h1>
       </div>
       <FormGroup>
-        <FormControlLabel
-          control={<Switch checked={this.state.card} onChange={() =>{this.setState ({card : !this.state.card});}} />}
+        <FormControlLabel 
+          className={style.switch}
+          control={<BlueSwitch className={style.switch2}  checked={this.state.card} onChange={() =>{this.setState ({card : !this.state.card});}} />}
           label="Card"
         />
       </FormGroup>
