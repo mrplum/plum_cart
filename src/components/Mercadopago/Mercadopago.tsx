2e7ea79f0eb9a0717b1935f2b76a819d180787f4
Luciano Rocha <lucho@plum.com.ar>
2020-12-02 03:55:35 -0300

    MercadoPago cleanup.

--- src/components/Mercadopago/Mercadopago.tsx
+++ src/components/Mercadopago/Mercadopago.tsx
@@ -1,74 +1,66 @@
-import React, { useContext, useEffect, useState } from "react";
-import mercadopago from "mercadopago";
-import Button from "../Button";
-import { Link } from "react-router-dom";
-import { useIntl } from "react-intl";
-import { CartContext } from "../../context";
-import style from "./Mercadopago.module.css";
+import React, { useContext, useEffect, useState, useCallback } from 'react';
+import mercadopago from 'mercadopago';
+import Button from '../Button';
+import { Link } from 'react-router-dom';
+import { useIntl } from 'react-intl';
+import { CartContext } from '../../context';
+import style from './Mercadopago.module.css';
+
+const sandbox = true;
+const access_token = 'TEST-1807600686871209-112614-729d1dbd3c5e18b0dd6bf00117ad00b6-678201171';
 
 const Mercadopago = (): JSX.Element => {
   const { state } = useContext(CartContext);
-  const [s, setState] = useState({ bol: false, dir: "" });
+  const [s, setState] = useState({ bol: false, dir: '' });
 
-  const items = state.list;
-
-  const payer = {
-    email: "test_user_69999056@testuser.com",
-  };
-
-  const configureMercadoPago = () => {
+  const set = useCallback(async () => {
     try {
-      mercadopago.configure({
-        sandbox: true,
-        access_token:
-          "TEST-1807600686871209-112614-729d1dbd3c5e18b0dd6bf00117ad00b6-678201171",
-      });
+      const response = await fetch(
+        'https://api.mercadopago.com/checkout/preferences',
+        {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${access_token}`,
+          },
+          body: JSON.stringify({
+            items: state.list,
+            payer: { email: 'test_user_69999056@testuser.com' },
+          }),
+        }
+      );
+      
+      const jsonResponse = await response.json();
+      setState({ bol: true, dir: jsonResponse.init_point });
     } catch (error) {
       console.log(error);
     }
-  };
 
-  const set = () => {
-    fetch(
-      "https://api.mercadopago.com/checkout/preferences?access_token=TEST-1807600686871209-112614-729d1dbd3c5e18b0dd6bf00117ad00b6-678201171",
-      {
-        method: "POST",
-        body: JSON.stringify({ items, payer }),
-        headers: {
-          "content-type": "application/json",
-          Authorization: "Bearer ACCESS_TOKEN_ENV",
-        },
-      }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        setState({ bol: true, dir: response.init_point });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  }, [state.list]);
 
   useEffect(() => {
     set();
-  }, [state.list]); //cambiar cuando cambien los elementos del carrito
+  }, [state.list]);
 
-  configureMercadoPago();
+  useEffect(() => {
+    try {
+      mercadopago.configure({ sandbox, access_token });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <div className={style.containerButton}>
-      {s.bol ? (
+      {s.bol && (
         <Link to={{ pathname: s.dir }} target="_blank">
           <Button
-            label={useIntl().formatMessage({ id: "pay" })}
+            label={useIntl().formatMessage({ id: 'pay' })}
             dark={true}
             primary={true}
             large={true}
           />
         </Link>
-      ) : (
-        <div></div>
       )}
     </div>
   );
